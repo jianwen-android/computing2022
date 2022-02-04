ce6cd5bdf957fde8e7458443a29984a27459202c
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2022-02-04 11:41:34 +0800

    model version1 (predicts B)

--- python/GUIMockUpA.py
+++ python/GUIMockUpA.py
@@ -1,12 +1,24 @@
 import PySimpleGUI as sg
+import threading
+import time
 
-sg.theme('DarkBrown4')  # Add a touch of color
-
+#Do setup
 images = ["../assets/signA2.png", "../assets/signB2.png", "../assets/signG2.png", "../assets/signI2.png",
           "../assets/signL2.png"]
-
+isReceiving = False
 
 # All the stuff inside your window.
+
+def retrieveData():
+    # retrieveData
+    print('wenis')
+    count = 0
+    while isReceiving:
+        print('in loop')
+        time.sleep(1)
+        count += 1
+        updateImg(images[count % 4])
+
 
 def updateImg(src=None):
     window["signImg"].update(source=src, size=imgSz)
@@ -16,6 +28,7 @@ def updateText(text="Placeholder text"):
     window["outputText"].update(text)
 
 
+sg.theme('DarkBrown4')
 btnSz = (20, 3)
 imgSz = (530, 550)
 imgSrc = "../assets/placeholder2.png"
@@ -34,20 +47,23 @@ layout = [[sg.Column(buttonCol, element_justification='c'), sg.Column(col2, elem
 
 # Create the Window
 window = sg.Window('Cripple enabler', layout, default_element_size=(45, 1))
-# Event Loop to process "events" and get the "values" of the inputs
 
+# Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
+    t1 = threading.Thread(target=retrieveData, args=())
     if event == sg.WIN_CLOSED:  # if user closes window or clicks cancel
         break
     elif event == "_START_":
         print("Start")
+        isReceiving = True
+        t1.start()
         # run start()
 
     elif event == "_STOP_":
         print("Stop")
         # run stop()
-
+        isReceiving = False
 
     elif event == "_CALIBRATE_":
         print("Calibrate")
