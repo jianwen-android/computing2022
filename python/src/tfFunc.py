86c517c3d1b550fdf4c00cfe30de7deb700fe49b
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2022-02-04 12:08:24 +0800

    model version1 (predicts B)

---
+++ python/src/tfFunc.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import os
+
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q",
+               "R", "S", "T", "U", "W", "X", "Y"]
+
+def setupModel():
+
+    os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dense(23, activation="softmax")
+    ])
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.load_weights("src/weights/weights1")
+
+    pDf = pd.read_csv("src/pData.csv")
+
+    pX = pd.get_dummies(pDf.drop(["Letter"], axis=1))
+    # temp data
+    return model,pX
+
+
+def modelPredict(model,pX):
+    # setup complete
+    predictions = model.predict(pX)
+    classes = np.argmax(predictions, axis=1)
+    print(letters[classes[0]])
+    return letters[classes[0]]
